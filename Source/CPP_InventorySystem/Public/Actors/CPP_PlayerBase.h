3fdda336754470e1903916062f192a2c3435c1e8
may <32318838+Duchess-May@users.noreply.github.com>
2024-05-08 00:28:05 -0700

    Inventory Updates
    1. Dupe glitch fixed by only allowing one confirm window when dropping items
    2. Added Tooltip to items
    3. Split NPC and Player to have different base CPP classes

---
+++ Source/CPP_InventorySystem/Public/Actors/CPP_PlayerBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Actors/CPP_CharacterBase.h"
+#include "CPP_PlayerBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CPP_INVENTORYSYSTEM_API ACPP_PlayerBase : public ACPP_CharacterBase
+{
+	GENERATED_BODY()
+	
+};
